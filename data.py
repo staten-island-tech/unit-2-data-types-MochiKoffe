22c203ddc720736763243afe20e6ebc91b771681
MochiKoffe <sophiem47@nycstudents.net>
2026-02-03 10:17:10 -0500

    Day 6

--- data.py
+++ data.py
@@ -22,7 +22,44 @@ if day_of_week == "Friday":
 else: 
     print("incorrect") """
 
+""" number = int(input("give me number"))
 
+if number >= 21: 
+    print("can vote and gamble")
+elif(number >= 18): 
+    print("can vote but not gamble")
+else: 
+    print("have a juicebox kid") """
 
-sentence = input("type a sentence")
-if sentence == input
+""" temp = 55
+if (temp > 40 and temp < 60):
+    print("lightjacket needed")
+elif (temp <20 or temp > 90): 
+    print("extreme") """
+
+""" def discount(isMember, age, isResident):
+    if (isMember or isResident):
+        print("discount granted")
+age = int(input("enter age"))
+if age >= 65:
+    print("discount granted")
+elif(age < 12): 
+    print("discount granted")
+else: 
+    print("NO DISCOUNT") """
+
+""" x = "test"
+print(f"hello {x}") """
+
+""" temp = 75
+if temp > 68: 
+    print("warm")
+elif temp == 68: 
+    print("perfect")
+else: 
+    print("cold") """
+
+def total(bill, tip):
+    bill = float(input("enter bill"))
+    print(bill)
+    tip = int(input("how much do you want to tip"))
